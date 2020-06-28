9bfd2349cc45fc1f246998c57a419e11dd837e0c
Hamish Wadsworth <hamish@swipedon.com>
2020-06-28 20:13:22 +1200

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <img alt="Vue LOGO" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js APP"/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
+    Navbar,
     HelloWorld,
   },
 };
